57fc09497b534e75f55e29f9e029049ba15d94c5
Sandro <you@example.com>
2023-12-09 22:55:53 -0300

    remove previous ignores and set variable names starting with _

--- src/send_queues/base.ts
+++ src/send_queues/base.ts
@@ -8,33 +8,30 @@ class BaseSendQueue implements ISendQueue {
    * @param canSend Function which should return whether send can be called
    * @note If parameters are not passed, initialize() should be called later.
    */
-  constructor(sendNow?: (bytes: string) => number, canSend?: () => boolean) {
+  constructor(sendNow?: (_bytes: string) => number, canSend?: () => boolean) {
     if (sendNow && canSend) {
       this.initialize(sendNow, canSend);
     } else if (sendNow || canSend) {
       throw new Error('Both sendNow and canSend must be provided, or neither must.');
     }
   }
-  //ts-ignore
-  public sendNow: (bytes: string) => number = (bytes) => -1;
+  public sendNow: (_bytes: string) => number = (_bytes) => -1;
   public canSend: () => boolean = () => false;
 
   /**
    * @param sendNow Function to call to send a message
    * @param canSend Function which should return whether send can be called
    */
-  public initialize(sendNow: (bytes: string) => number, canSend: () => boolean) {
+  public initialize(sendNow: (_bytes: string) => number, canSend: () => boolean) {
     this.sendNow = sendNow;
     this.canSend = canSend;
   }
 
-  //ts-ignore
-  public send(bytes: string): number {
+  public send(_bytes: string): number {
     throw new Error('not implemented');
   }
 
-  //ts-ignore
-  public queueMessage(bytes: string): boolean {
+  public queueMessage(_bytes: string): boolean {
     throw new Error('not implemented');
   }
   public processQueue(): number {
